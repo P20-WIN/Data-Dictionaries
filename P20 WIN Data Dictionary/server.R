f63ee0234cd907bce0740156647f6ad9e96fbe34
Coral Wonderly <103579521+coral-wonderly@users.noreply.github.com>
2023-02-23 08:21:19 -0500

    updated ui and server files

--- P20 WIN Data Dictionary/server.R
+++ P20 WIN Data Dictionary/server.R
@@ -1,4 +1,8 @@
-#### Installing Packages ####
+## P20 WIN DATA DICTIONARY SERVER SCRIPT ##
+# The Server Script contains the instructions for how to build your filters and table (i.e. how they interact and what data to display)
+# Shiny Tutorials and other resources: https://shiny.rstudio.com/tutorial/
+
+### Loading Required Packages (install may be required) ####
 library(shiny)
 library(shinyWidgets)
 library(readxl)
@@ -10,19 +14,25 @@ library(DT)
 library(htmlwidgets)
 thematic::thematic_shiny(font = "auto")
 
-
 #### Reading in the Data Dictionary ####
 P20WIN_Data_Dictionary <- read_excel("P20WIN_Data_Dictionary.xlsx")
 
 #### Server Script ####
 server <- function(session, input, output) {
   
-  session$onSessionEnded(function() {
-    stopApp()
-  })
-    
-  #bs_themer()
+  # The following chunk is helpful when you are testing your app before publishing. If you don't include this your app may not stop running if there are 
+  # issues in your script. This function ensures that if you close the app the script will stop running.
   
+  #session$onSessionEnded(function() {
+  #  stopApp()
+  # })
+  
+  # The following section is taking the input filters created in the ui file and making them reactive to each other. 
+  # These filters do need to have a set hierarchy, meaning they should go from most generic to most specific, in this case agency -> program -> data category.
+  
+  # The first filter event is if the agency filter is used, when this event is observed, the available choices for both the program and data category filters
+  # are updated to only display the choices that correspond to the agency/agencies selected. 
+
   agencies <- reactive({
     filter(P20WIN_Data_Dictionary, Agency %in% input$select_agency)
   })
@@ -39,6 +49,9 @@ server <- function(session, input, output) {
                       selected = choices_2)
   })
   
+  # The second filter event is if the program filter is used, when this event is observed, the available choices for the data category filter
+  # are updated to only display the choices that correspond to the program/programs selected. 
+  
   programs <- reactive({
     filter(agencies(), Program %in% input$select_program)
   })
@@ -50,6 +63,12 @@ server <- function(session, input, output) {
                       selected = choices_3)
   })
   
+  # It should be noted that filters will not work in the other direction. If you create have a "select agency" filter that updates the program filter then you cannot have
+  # a "select program" filter that updates the agency filter because you will end up with a never ending loop where both filters are simultaneously trying to update the other. 
+  
+  
+  # The rest of the script is setting up the data table/data dictionary.
+  # The first six lines are naming the input filters as data filters for the table.
 
   output$mytable <- DT::renderDataTable({
     req(input$select_program)
@@ -59,28 +78,30 @@ server <- function(session, input, output) {
       filter(Program %in% input$select_program)
     },
     escape = 0,
-    options = list(                
-                scrollX = FALSE,   ## enable scrolling on X axis
-                scrollY = '50vh',   ## enable scrolling on Y axis
-                autoWidth = FALSE,
+    options = list(
+                scrollX = FALSE, ## disable scrolling on X axis
+                scrollY = '64vh',   ## enable scrolling on Y axis and setting table height to 64% of the screens verticle height. This will depend on how tall the navbar or header is.
+                autoWidth = FALSE, ## disable auto column widths (set below)
                 columnDefs = list(
-                  list(targets = c(0), visible = TRUE, width = '50px',
+                  list(targets = c(0), visible = TRUE, width = '60px', ## this column will work as a button to expand the row to show hidden fields (see callback = JS below)
                        orderable = FALSE, className = 'details-control'), 
-                  list(targets = c(1,2,3,4), visible = TRUE, width='89px'), 
+                  list(targets = c(1,2,3), visible = TRUE, width='89px'), 
+                  list(targets = c(4), visible = TRUE, width='215px'), 
                   list(targets = c(5), visible = TRUE), 
-                  list(targets = c(6,7), visible = FALSE, width = '0px')), 
-                paging = FALSE,    ## paginate the output
-                pageLength = 2000, ## number of rows to output for each page
+                  list(targets = c(6,7), visible = FALSE, width = '0px')), ## hiding the columns that will be shown when the expansion button is clicked
+                paging = FALSE,    ## disable paginate so that the table shows as a continuous scroll
+                pageLength = 200000, ## number of rows to output for each page. We chose a large number that should not ever be reached to ensure no elements are dropped.
                 server = FALSE,   ## use client-side processing
                 dom = 'Bfrt',
-                buttons = list(
+                buttons = list( ## adding download buttons for CSV and Excel documents. Only the selected elements will be downloaded. 
                   list(extend = 'csv', title = NULL, exportOptions = list(columns = c(1:7)),
                        filename = "P20_WIN_Data_Dictionary"), 
                   list(extend = 'excel', title = NULL, exportOptions = list(columns = c(1:7)),
                         filename = "P20_WIN_Data_Dictionary")
                  )
                 ),
-    callback = JS("
+    ## custom javascript to create the expansion button and show the hidden child rows. 
+    callback = JS(" 
   table.column(0).nodes().to$().css({cursor: 'pointer'});
   var format = function(d) {
     return '<div style=\"background-color:#Ffffff; padding: .5em;\"> Data Type: ' +
--- P20 WIN Data Dictionary/ui.R
+++ P20 WIN Data Dictionary/ui.R
@@ -1,4 +1,10 @@
-#### Installing Packages ####
+## P20 WIN DATA DICTIONARY USER INTERFACE SCRIPT ##
+# The UI Script is used to set up the layout and appearance of your shiny app including pages, widgets, and themes.
+# The files necessary should be saved in their own folder with no other files. Your files also cannot call on data from different folders.
+# Everything must be contained within the same file directory.
+# Shiny Tutorials and other resources: https://shiny.rstudio.com/tutorial/
+
+#### Loading Required Packages (install may be required) ####
 library(shiny)
 library(shinyWidgets)
 library(readxl)
@@ -10,74 +16,133 @@ library(DT)
 library(htmlwidgets)
 thematic::thematic_shiny(font = "auto")
 
-#### UI Script ####
-
 #### Reading in the Data Dictionary ####
+
+# Data is initially loaded to build the input filters found below. 
+# However, most manipulation of the data should be done in the Server script.
+
 P20WIN_Data_Dictionary <- read_excel("P20WIN_Data_Dictionary.xlsx")
 
-#The UI script is used to set the themes, fonts, and layout of the page. 
+#### UI Script ####
+## Setting the page theme ##
+# To see more themes available through Bootswatch: https://bootswatch.com/ 
+# You can also create a custom theme using a .css file saved in the same file directory as your ui.R and Server.R files.
 
-ui <- fluidPage(title="P20 WIN Data Dictionary",theme = bs_theme(
+## Setting the page theme ##
+P20_theme <- bs_theme(
+  version = version_default(),
   bootswatch = "yeti",
   bg = "#ffffff",
-  fg = "#054266",
-  primary =  "#054266",
-  secondary = "#0771BB",
-  base_font = font_google("Poppins"),
-  heading_font = font_google("Poppins"),
+  fg =  "#054266",
+  primary = "#053955",
+  secondary = "#0071BB",
+  base_font = font_google("Open Sans"),
+  heading_font = font_google("Open Sans"),
   font_scale = 1
-),
-titlePanel(
-  #This panel contains the title and P20 WIN logo
-  fluidRow(
-    column(4, h1("P20 WIN Data Dictionary", align = "left",style = "padding-bottom:30px;")),
-    column(4, offset = 4, img(width = 190, src = "P20WIN_logo.png", align = "Right", style = "padding-top:25px;"))
-    )
-  ),
-sidebarLayout(
-  #This panel includes the three filters for agency, program, and category. They are all multiple 
-  #selection "pickerInput" filters 
-  sidebarPanel(width = 3,
-               pickerInput("select_agency",
-                           label = "Select Agencies",
-                           choices = as.list(unique(P20WIN_Data_Dictionary$Agency)),
-                           selected = unique(P20WIN_Data_Dictionary$Agency),
-                           options = list(`actions-box` = TRUE,
-                                          `count-selected-text` = "{0}/{1} Selected"),
-                           multiple = TRUE
-                           ),
-               pickerInput("select_program",
-                           label = "Select Programs",
-                           choices = as.list(unique(P20WIN_Data_Dictionary$Program)),
-                           selected = unique(P20WIN_Data_Dictionary$Program),
-                           options = list(`actions-box` = TRUE,
-                                          `count-selected-text` = "{0}/{1} Selected"),
-                           multiple = TRUE
-                           ),
-               pickerInput("select_category",
-                           label = "Select Data Categories",
-                           choices = as.list(unique(P20WIN_Data_Dictionary$`Data Category`)),
-                           selected = unique(P20WIN_Data_Dictionary$`Data Category`),
-                           options = list(`actions-box` = TRUE,
-                                          `count-selected-text` = "{0}/{1} Selected"),
-                           multiple = TRUE
-                           )
-               ),
-  mainPanel(
-    #This panel contains the data dictionary table.
-    width = 9,
-    height = 100,
-    tags$head(
-      tags$style(HTML("table {table-layout: fixed;"))
-      ),
-    DT::dataTableOutput("mytable"),
-    #tags$hr(style = "border-top: 1px solid #648290;
-    #        width: 940px;")
-    )
-  ),
-fluidRow(
-  #This row is used to show what date the data dictionary was last updated
-  column(8, h6("last updated: 11/29/2022", align = "left"))
-)
 )
 
+#capturing the date to include at the bottom of page 
+today <- Sys.Date()
+today <-format(today, format="%B %d %Y")
+
+## App and Page Layout ##
+# If you are including any photos or logos they must be saved within a folder titled "www" within your app's file directory.
+
+shinyUI(
+  #our app is currently setup with a navigation bar and multiple pages. If you only need one page, you should use fluidPage instead.
+  navbarPage(
+    #creating the title that lives on the navbar with our logo and name.
+    title = div(img(width = 40, src = "P20WINlogo_color_noname.png", style = "padding-bottom:5px;"),
+                "P20 WIN", style = "padding-top:40px; padding-right:0px;", 
+              ), 
+    #setting the page theme
+    theme = P20_theme,
+    #HTML is used here to set the height and padding for the navbar.
+    tags$head(
+      tags$style(HTML(' .navbar {
+                          height: 60px;
+                          min-height:25px !important;
+                        }
+                      .navbar-nav > li > a, .navbar-brand {
+                            padding-top:1px !important; 
+                            padding-bottom:1px !important;
+                            height: 25px;
+                            font-size: 25px;
+                            }'))),
+    #These are the pages that will be located in the navbar. We use a regular tabPanel for the first page and then a
+    #navbarMenu page with two tabPanel pages nested inside. 
+             tabPanel(
+               "Data Dictionary",
+               fluidPage(title="Data Dictionary",
+                         theme = P20_theme
+                         ),
+               #This sidebar panel includes the three filters for agency, program, and category. 
+               #They are all multiple selection "pickerInput" filters
+               sidebarLayout(
+                 sidebarPanel(width = 3,
+                              pickerInput("select_agency",
+                                          label = "Select Agencies",
+                                          choices = as.list(unique(P20WIN_Data_Dictionary$Agency)),
+                                          selected = unique(P20WIN_Data_Dictionary$Agency),
+                                          options = list(`actions-box` = TRUE,
+                                                         `count-selected-text` = "{0}/{1} Selected"),
+                                          multiple = TRUE
+                                          ),
+                              pickerInput("select_program",
+                                          label = "Select Programs",
+                                          choices = as.list(unique(P20WIN_Data_Dictionary$Program)),
+                                          selected = unique(P20WIN_Data_Dictionary$Program),
+                                          options = list(`actions-box` = TRUE,
+                                                         `count-selected-text` = "{0}/{1} Selected"),
+                                          multiple = TRUE
+                                          ),
+                              pickerInput("select_category",
+                                          label = "Select Data Categories",
+                                          choices = as.list(unique(P20WIN_Data_Dictionary$`Data Category`)),
+                                          selected = unique(P20WIN_Data_Dictionary$`Data Category`),
+                                          options = list(`actions-box` = TRUE,
+                                                         `count-selected-text` = "{0}/{1} Selected"),
+                                          multiple = TRUE
+                                          )
+                              ),
+                 #This panel contains the data dictionary table.
+                 mainPanel(
+                   width = 9,
+                   height = 100,
+                   #HTML is used here to hide the horizontal scroll bar
+                   tags$table(
+                     tags$style(HTML("table {table-layout: fixed;
+                                     overflow: hidden;"))
+                     ),
+                   #we will assign data to "mytable" in the server.R file. 
+                   DT::dataTableOutput("mytable"),
+                   )
+                 ),
+               #This row is used to show what date the data dictionary was last updated
+               fluidRow(
+                 column(8, h6("last updated:", today, align = "left"))
+                 )
+               ),
+            navbarMenu(
+              "Data Landscapes",
+               tabPanel(
+                 "Agencies",
+                 fluidPage(title="Agency Data Landscapes",
+                           theme = P20_theme
+                           ),
+                 mainPanel(
+                   p("Agency Data Landscapes are currently in development.")
+                   )
+                 ),
+              tabPanel(
+                "Topic Areas",
+                fluidPage(title="Topic Area Data Landscapes",
+                          theme = P20_theme
+                          ),
+                mainPanel(
+                  p("Topic Area Data Landscapes are currently in development.")
+                  )
+                )
+              )
+    )
+  )
