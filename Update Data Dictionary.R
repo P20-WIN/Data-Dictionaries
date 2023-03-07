a0c9a71eb272d72289963d40f5100ba103b25f5f
Coral Wonderly <103579521+coral-wonderly@users.noreply.github.com>
2023-03-07 15:32:35 -0500

    Adding Adult Education elements

--- Update Data Dictionary.R
+++ Update Data Dictionary.R
@@ -3,16 +3,16 @@ library(readxl)
 library(xlsx)
 
 #### Reading in the Data Dictionaries - add row for new agencies ####
-CCEH <- read_excel("~/Data-Dictionaries/Data Dictionaries (Upload Here)/CCEH/CCEH_Data_Dictionary.xlsx")
-CCIC <- read_excel("~/Data-Dictionaries/Data Dictionaries (Upload Here)/CCIC/CCIC_Data_Dictionary.xlsx")
-CSCU <- read_excel("~/Data-Dictionaries/Data Dictionaries (Upload Here)/CSCU/CSCU_Data_Dictionary.xlsx")
-DCF <- read_excel("~/Data-Dictionaries/Data Dictionaries (Upload Here)/DCF/DCF_Data_Dictionary.xlsx")
-DHMAS <- read_excel("~/Data-Dictionaries/Data Dictionaries (Upload Here)/DMHAS/DMHAS_Data_Dictionary.xlsx")
-DOL <- read_excel("~/Data-Dictionaries/Data Dictionaries (Upload Here)/DOL/DOL_Data_Dictionary.xlsx")
-OEC <- read_excel("~/Data-Dictionaries/Data Dictionaries (Upload Here)/OEC/OEC_Data_Dictionary.xlsx")
-OHE <- read_excel("~/Data-Dictionaries/Data Dictionaries (Upload Here)/OHE/OHE_Data_Dictionary.xlsx")
-SDE <- read_excel("~/Data-Dictionaries/Data Dictionaries (Upload Here)/SDE/SDE_Data_Dictionary.xlsx")
-UConn <- read_excel("~/Data-Dictionaries/Data Dictionaries (Upload Here)/UConn/UConn_Data_Dictionary.xlsx")
+CCEH <- read_excel("Data Dictionaries (Upload Here)/CCEH/CCEH_Data_Dictionary.xlsx")
+CCIC <- read_excel("Data Dictionaries (Upload Here)/CCIC/CCIC_Data_Dictionary.xlsx")
+CSCU <- read_excel("Data Dictionaries (Upload Here)/CSCU/CSCU_Data_Dictionary.xlsx")
+DCF <- read_excel("Data Dictionaries (Upload Here)/DCF/DCF_Data_Dictionary.xlsx")
+DHMAS <- read_excel("Data Dictionaries (Upload Here)/DMHAS/DMHAS_Data_Dictionary.xlsx")
+DOL <- read_excel("Data Dictionaries (Upload Here)/DOL/DOL_Data_Dictionary.xlsx")
+OEC <- read_excel("Data Dictionaries (Upload Here)/OEC/OEC_Data_Dictionary.xlsx")
+OHE <- read_excel("Data Dictionaries (Upload Here)/OHE/OHE_Data_Dictionary.xlsx")
+SDE <- read_excel("Data Dictionaries (Upload Here)/SDE/SDE_Data_Dictionary.xlsx")
+UConn <- read_excel("Data Dictionaries (Upload Here)/UConn/UConn_Data_Dictionary.xlsx")
 
 #### Creating P20 WIN Data Dictionary ####
 P20WIN_Data_Dictionary <- rbind(CCEH,CCIC,CSCU,DCF,DHMAS,DOL,OEC,OHE,SDE,UConn)
@@ -21,4 +21,4 @@ P20WIN_Data_Dictionary <- P20WIN_Data_Dictionary[,c(8,1:7)] #moving the added co
 P20WIN_Data_Dictionary[is.na(P20WIN_Data_Dictionary)] <- "Not Available"
 
 #### Saving P20 WIN Data Dictionary ####
-write.xlsx(as.data.frame(P20WIN_Data_Dictionary),"~/Data-Dictionaries/P20 WIN Data Dictionary/P20WIN_Data_Dictionary.xlsx", row.names = FALSE)
+write.xlsx(as.data.frame(P20WIN_Data_Dictionary),"P20 WIN Data Dictionary/P20WIN_Data_Dictionary.xlsx", row.names = FALSE)
